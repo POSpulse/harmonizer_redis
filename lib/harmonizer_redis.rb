c5f83b6f22c5c85cce1ce4a43577e03efe52535f
Tian Wang <twang95@stanford.edu>
2015-09-04 14:18:49 +0200

    Refactor harmonizer_redis.rb, IdfScorer

--- lib/harmonizer_redis.rb
+++ lib/harmonizer_redis.rb
@@ -13,37 +13,47 @@ include WhiteSimilarity
 
 module HarmonizerRedis
   ### Calculate Similarities. Store them with the category
-  def HarmonizerRedis.calculate_similarities(category_id)
-    unless Category.valid?(category_id)
-      raise "Category ID: #{category_id} is invalid"
-    end
+  class << self
+    def calculate_similarities(category_id)
+      unless Category.valid?(category_id)
+        raise "Category ID: #{category_id} is invalid"
+      end
 
-    phrase_id_list = Category.get_phrase_list(category_id)
+      phrase_id_list = Category.get_phrase_list(category_id)
 
-    matrix_list = Category.get_matrices(category_id, phrase_id_list)
+      matrix_list = Category.get_matrices(category_id, phrase_id_list)
 
-    Redis.current.pipelined do
-      (0...phrase_id_list.length).each do |i|
-        (i + 1...phrase_id_list.length).each do |j|
-          id_x = phrase_id_list[i]
-          id_y = phrase_id_list[j]
-          score = Phrase.calc_soft_pair_similarity(matrix_list[i], matrix_list[j])
-          unless score < 0.2
-            Redis.current.zadd("HarmonizerRedis::Category:#{category_id}:#{id_x}:sims", score, id_y)
-            Redis.current.zadd("HarmonizerRedis::Category:#{category_id}:#{id_y}:sims", score, id_x)
+      Redis.current.pipelined do
+        (0...phrase_id_list.length).each do |i|
+          (i + 1...phrase_id_list.length).each do |j|
+            id_x = phrase_id_list[i]
+            id_y = phrase_id_list[j]
+            score = Phrase.calc_soft_pair_similarity(matrix_list[i], matrix_list[j])
+            unless score < 0.2
+              add_similarity_entry(id_x, id_y, score, category_id)
+            end
           end
         end
       end
+
+      Category.reset_changed(category_id)
     end
 
-    Category.reset_changed(category_id)
-  end
 
+    ### String PreProcessing
+    def normalize_string(string)
+      ActiveSupport::Inflector.transliterate(string.strip.downcase).
+          split(/[^\p{L}0-9]/).delete_if { |x| x.length == 0 }.join(' ')
+    end
 
-  ### String PreProcessing
-  def HarmonizerRedis.normalize_string(string)
-    ActiveSupport::Inflector.transliterate(string.strip.downcase).
-        split(/[^\p{L}0-9]/).delete_if { |x| x.length == 0 }.join(' ')
+    ### Helper
+
+    def add_similarity_entry(id_x, id_y, score, category_id)
+      Redis.current.zadd("HarmonizerRedis::Category:#{category_id}:#{id_x}:sims", score, id_y)
+      Redis.current.zadd("HarmonizerRedis::Category:#{category_id}:#{id_y}:sims", score, id_x)
+    end
+
+    private :add_similarity_entry
   end
 
 end
--- lib/harmonizer_redis/idf_scorer.rb
+++ lib/harmonizer_redis/idf_scorer.rb
@@ -1,110 +1,113 @@
 module HarmonizerRedis
   module IdfScorer
-    def IdfScorer.add_document(phrase_id)
-      IdfScorer.incr_doc_count
-      text = HarmonizerRedis::Phrase.get_content(phrase_id)
-      word_set = Set.new
-      text.split.each do |word|
-        unless word_set.include? word
-          word_set.add(word)
-          Redis.current.incr(word_doc_freq_key(word))
-        end
-        Redis.current.incr(word_count_key(word))
-      end
-    end
-
-    def IdfScorer.get_score(word)
-      doc_freq = IdfScorer.get_doc_freq(word) + 0.1
-      doc_count = IdfScorer.doc_count + 0.1
-      Math.log(0.1+(doc_count / doc_freq))
-    end
-
-    # first char is length of phrase (capped at 255 words). Each word(key) is separated
-    # by a comma and the keys and values are separated by a vertical bar
-    def IdfScorer.serialize_matrix(matrix)
-      serialized = "#{matrix.length.chr}#{matrix.keys.join(',')},#{matrix.values.join(',')}"
-      serialized
-    end
-
-    # Used for soft cosine similarity
-    def IdfScorer.calc_soft_matrix(phrase_content)
-      matrix = Hash.new(0.0)
-      phrase_content.split.each do |word|
-        matrix[word] += 1.0
-      end
-
-      matrix.each do |word, count|
-        updated = (1.0 + Math::log10(count)) * IdfScorer.get_score(word)
-        matrix[word] = updated
-      end
-
-      #calculate normalization factor
-      norm_factor_sqrd = 0.0
-      matrix.each do |word_a, value_a|
-        matrix.each do |word_b, value_b|
-          similarity = WhiteSimilarity.score(word_a, word_b)
-          norm_factor_sqrd += (similarity * value_a * value_b)
-        end
-      end
-
-      #normalize
-      factor = Math::sqrt(norm_factor_sqrd)
-      matrix.each do |word, value|
-        matrix[word] = value / factor
-      end
-      matrix
-    end
-
-    def IdfScorer.cos_similarity(matrix_a, matrix_b)
-      similarity = 0.0
-      matrix_a.each do |word, value|
-        similarity += (value * matrix_b[word])
-      end
-      similarity
-    end
-
-    def IdfScorer.soft_cos_similarity(matrix_a, matrix_b)
-      similarity = 0.0
-      matrix_a.each do |word_a, value_a|
-        matrix_b.each do |word_b, value_b|
-          if word_a != word_b
-            white_similarity = WhiteSimilarity.score(word_a, word_b)
-          else
-            white_similarity = 1.0
+    # class self
+    class << self
+      def add_document(phrase_id)
+        self.incr_doc_count
+        text = HarmonizerRedis::Phrase.get_content(phrase_id)
+        word_set = Set.new
+        text.split.each do |word|
+          unless word_set.include? word
+            word_set.add(word)
+            Redis.current.incr(word_doc_freq_key(word))
           end
-          similarity += (white_similarity * value_a * value_b)
+          Redis.current.incr(word_count_key(word))
         end
       end
-      similarity
-    end
 
-    def IdfScorer.get_doc_freq(word)
-      Redis.current.get(word_doc_freq_key(word)).to_f
-    end
+      def get_score(word)
+        doc_freq = self.get_doc_freq(word) + 0.1
+        doc_count = self.doc_count + 0.1
+        Math.log(0.1+(doc_count / doc_freq))
+      end
 
-    def IdfScorer.get_count(word)
-      Redis.current.get(word_count_key(word)).to_f
-    end
+      # first char is length of phrase (capped at 255 words). Each word(key) is separated
+      # by a comma and the keys and values are separated by a vertical bar
+      def serialize_matrix(matrix)
+        serialized = "#{matrix.length.chr}#{matrix.keys.join(',')},#{matrix.values.join(',')}"
+        serialized
+      end
 
-    def IdfScorer.decr_doc_freq(word)
-      Redis.current.decr(word_doc_freq_key(word))
-      Redis.current.decr("#{self}:doc_count")
-    end
+      # Used for soft cosine similarity
+      def calc_soft_matrix(phrase_content)
+        matrix = Hash.new(0.0)
+        phrase_content.split.each do |word|
+          matrix[word] += 1.0
+        end
 
-    def IdfScorer.doc_count
-      Redis.current.get("#{self}:doc_count").to_f
-    end
+        matrix.each do |word, count|
+          updated = (1.0 + Math::log10(count)) * self.get_score(word)
+          matrix[word] = updated
+        end
 
-    def IdfScorer.incr_doc_count
-      Redis.current.incr("#{self}:doc_count")
-    end
+        #calculate normalization factor
+        norm_factor_sqrd = 0.0
+        matrix.each do |word_a, value_a|
+          matrix.each do |word_b, value_b|
+            similarity = WhiteSimilarity.score(word_a, word_b)
+            norm_factor_sqrd += (similarity * value_a * value_b)
+          end
+        end
 
-    def IdfScorer.word_doc_freq_key(word)
-      "Word:[#{word}]:doc_freq"
-    end
+        #normalize
+        factor = Math::sqrt(norm_factor_sqrd)
+        matrix.each do |word, value|
+          matrix[word] = value / factor
+        end
+        matrix
+      end
 
-    def IdfScorer.word_count_key(word)
-      "Word:[#{word}]:count"
+      def cos_similarity(matrix_a, matrix_b)
+        similarity = 0.0
+        matrix_a.each do |word, value|
+          similarity += (value * matrix_b[word])
+        end
+        similarity
+      end
+
+      def soft_cos_similarity(matrix_a, matrix_b)
+        similarity = 0.0
+        matrix_a.each do |word_a, value_a|
+          matrix_b.each do |word_b, value_b|
+            if word_a != word_b
+              white_similarity = WhiteSimilarity.score(word_a, word_b)
+            else
+              white_similarity = 1.0
+            end
+            similarity += (white_similarity * value_a * value_b)
+          end
+        end
+        similarity
+      end
+
+      def get_doc_freq(word)
+        Redis.current.get(word_doc_freq_key(word)).to_f
+      end
+
+      def get_count(word)
+        Redis.current.get(word_count_key(word)).to_f
+      end
+
+      def decr_doc_freq(word)
+        Redis.current.decr(word_doc_freq_key(word))
+        Redis.current.decr("#{self}:doc_count")
+      end
+
+      def doc_count
+        Redis.current.get("#{self}:doc_count").to_f
+      end
+
+      def incr_doc_count
+        Redis.current.incr("#{self}:doc_count")
+      end
+
+      def word_doc_freq_key(word)
+        "Word:[#{word}]:doc_freq"
+      end
+
+      def word_count_key(word)
+        "Word:[#{word}]:count"
+      end
     end
   end
 end
